8c0aaf7b11c67266d090889cc70a59f148ce2fbb
Holberton iMac 17 <holbertonimac17@Holbertons-iMac.local>
2024-02-16 16:30:40 -0500

    Fix lint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
-  if (data){
+  if (data) {
     process.stdout.write(`Your name is: ${data}`);
   }
   process.exit();
 });
 
 process.on('exit', () => {
- process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
